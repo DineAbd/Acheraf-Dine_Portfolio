945b16e5a2edc48ca67a252555726c71472065a1
DineAbd <dineaboudou8@gmail.com>
2024-09-12 22:14:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+function openResume(){
+    window.open('assets/CV-ABOUDOU _Acheraf_Dine-Fr.pdf');
+}
